79a0547bf04631831126d8d60847539581806742
Aaron Bond <aaronbond@outlook.com>
2017-08-31 09:55:02 +1000

    Add data classes

---
+++ src/main/kotlin/model/Owner.kt
@@ -0,0 +1,3 @@
+package main.kotlin.model
+
+data class Owner(val name: String, val age: Int, val gender: String, val pets: ArrayList<Pet>)
---
+++ src/main/kotlin/model/Pet.kt
@@ -0,0 +1,3 @@
+package main.kotlin.model
+
+data class Pet(val name: String, val type: String)
